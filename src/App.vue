a255d8e0add8fb18c4556ec00b44a41e1be4d974
alwalw <alwalw.qq.com>
2019-07-30 18:47:24 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+  <div class="todo-container">
+    <div class="todo-wrap">
+      <Header></Header>
+      <List></List>
+      <Footer></Footer>
+    </div>
+  </div>
+  
+</template>
+
+<script type="text/ecmascript-6">
+  import Header from './components/Header.vue'
+  import List from './components/List.vue'
+  import Footer from './components/Footer.vue'
+  
+</script>
+  
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+// 入口文件
+import Vue from 'vue'
+import App from './App.vue'
+
+/* 只能当前文件有效 */
+/* eslint-disable no-new */
+new Vue({
+  el:'#app',
+  components:{
+    App
+  },
+  template:'<App/>'
+})
